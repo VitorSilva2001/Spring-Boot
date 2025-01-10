4038c02d8d583d6682ce5e7c21a34fc3abb9083d
VitorSilva2001 <vitorbrito4@gmail.com>
2025-01-10 15:05:21 -0300

    Pull Request Criando o primeiro passo do Crud

--- eventoapp/src/main/java/com/evento/eventoapp/ControladorDoEvento.java
+++ eventoapp/src/main/java/com/evento/eventoapp/ControladorDoEvento.java
@@ -1,13 +1,27 @@
 package com.evento.eventoapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.evento.eventoapp.Modelos.Evento;
+import com.evento.eventoapp.Repositorio.RepositorioDoEvento;
 
 @Controller
 public class ControladorDoEvento {
 
-    @RequestMapping("/cadastroDoEvento")
-    public String form(){
-        return "Evento/formularioDoEvento";
+    @Autowired
+    private RepositorioDoEvento er;
+
+    @RequestMapping(value="/cadastroDoEvento", method=RequestMethod.GET)
+	public String form(){
+		return "Evento/formularioDoEvento";
+	}
+
+    @RequestMapping(value="/cadastroDoEvento", method=RequestMethod.POST)
+    public String form(Evento evento){
+        er.save(evento);
+        return "redirect:/cadastroDoEvento";
     }
 }
