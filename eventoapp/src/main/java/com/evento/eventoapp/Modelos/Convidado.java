7bd83d85b1b0eb87ea4b0c2524d3a26ec170ae19
VitorSilva2001 <vitorbrito4@gmail.com>
2025-02-02 13:57:22 -0300

    Organizando com o css Organização dos arquivos html: detalhesEvento e formularioDoEvento para que o css possa organizar os conteiners

--- eventoapp/src/main/java/com/evento/eventoapp/Modelos/Convidado.java
+++ eventoapp/src/main/java/com/evento/eventoapp/Modelos/Convidado.java
@@ -21,11 +21,11 @@ public class Convidado {
     public void setRg(String rg) {
         this.rg = rg;
     }
-    public String getNomeConvidado() {
-        return nomeConvidado;
-    }
-    public void setNomeConvidado(String nomeConvidado) {
-        this.nomeConvidado = nomeConvidado;
+        public String getNomeConvidado() {
+            return nomeConvidado;
+        }
+        public void setNomeConvidado(String nomeConvidado) {
+            this.nomeConvidado = nomeConvidado;
     }
     public Evento getEvento() {
         return evento;
